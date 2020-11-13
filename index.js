e0cd1f60ce668f83dedae11f62a4e470dad67418
Will Murphy <william@datatitian.com>
2020-11-13 13:59:12 -0600

    fix registration form validation to ensure compatibility with hubs, fix validation api to avoid half-created accounts (AP actor but no user) and provide informative errors

--- index.js
+++ index.js
@@ -87,19 +87,15 @@ app.get('/auth/logintoken', passport.authenticate('easy', {
 app.post('/auth/client', auth.registerClient)
 
 async function registerActor (req, res, next) {
-  const preferredUsername = req.body.username.toLowerCase()
+  const preferredUsername = req.body.username
   const name = req.body.name
-  apex.createActor(preferredUsername, name, 'immers profile')
-    .then(actor => apex.store.saveObject(actor))
-    .then(result => {
-      if (!result) {
-        return res.json({ taken: true })
-      }
-      next()
-    })
-    .catch(next)
+  try {
+    const actor = await apex.createActor(preferredUsername, name, 'immers profile')
+    await apex.store.saveObject(actor)
+    next()
+  } catch (err) { next(err) }
 }
-app.post('/auth/user', auth.logout, registerActor, auth.registration)
+app.post('/auth/user', auth.validateNewUser, auth.logout, registerActor, auth.registration)
 app.get('/auth/authorize', auth.authorization)
 app.post('/auth/decision', auth.decision)
 // get actor from token
