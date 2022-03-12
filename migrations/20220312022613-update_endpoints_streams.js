28ffffc536667357e713c00b9d7b37398d073a83
Will Murphy <william@datatitian.com>
2022-03-11 21:46:26 -0600

    avoid errors if actors have no previous endpoints/streams

--- migrations/20220312022613-update_endpoints_streams.js
+++ migrations/20220312022613-update_endpoints_streams.js
@@ -18,8 +18,8 @@ module.exports = {
           const actor = await apex.store
             .getObject(apex.utils.usernameToIRI(user.username), true)
           const tempActor = await createImmersActor(actor.preferredUsername[0], actor.name[0])
-          const endpoints = [Object.assign(actor.endpoints[0], tempActor.endpoints[0])]
-          const streams = [Object.assign(actor.streams[0], tempActor.streams[0])]
+          const endpoints = [Object.assign(actor.endpoints[0] || {}, tempActor.endpoints[0])]
+          const streams = [Object.assign(actor.streams[0] || {}, tempActor.streams[0])]
           await apex.store.updateObject({ id: actor.id, endpoints, streams }, actor.id, false)
           const newActor = await apex.store.getObject(actor.id, true)
           return apex.publishUpdate(newActor, newActor)
@@ -44,7 +44,6 @@ module.exports = {
         return Promise.all(users.map(async user => {
           const actor = await apex.store
             .getObject(apex.utils.usernameToIRI(user.username), true)
-          console.log(user.username, apex.utils.usernameToIRI(user.username), actor)
           const { endpoints, streams } = actor
           delete endpoints[0].proxyUrl
           delete streams[0].blocklist
