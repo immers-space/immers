db0522a25c6481aa636426dd007b89c0f2598dc5
Will Murphy <william@datatitian.com>
2022-03-11 21:36:11 -0600

    migration to update actor endpoints and streams with latest features

---
+++ migrations/20220312022613-update_endpoints_streams.js
@@ -0,0 +1,60 @@
+'use strict'
+require('dotenv').config()
+const { apex, createImmersActor } = require('../src/apex')
+
+module.exports = {
+  async up (db, client) {
+    apex.store.db = db
+    apex.offlineMode = true // outbox items are queued but not delivered
+
+    const users = await db.collection('users')
+      .find({})
+      .project({ _id: 0, username: 1 })
+      .toArray()
+    const session = client.startSession()
+    try {
+      await session.withTransaction(() => {
+        return Promise.all(users.map(async user => {
+          const actor = await apex.store
+            .getObject(apex.utils.usernameToIRI(user.username), true)
+          const tempActor = await createImmersActor(actor.preferredUsername[0], actor.name[0])
+          const endpoints = [Object.assign(actor.endpoints[0], tempActor.endpoints[0])]
+          const streams = [Object.assign(actor.streams[0], tempActor.streams[0])]
+          await apex.store.updateObject({ id: actor.id, endpoints, streams }, actor.id, false)
+          const newActor = await apex.store.getObject(actor.id, true)
+          return apex.publishUpdate(newActor, newActor)
+        }))
+      })
+    } finally {
+      await session.endSession()
+    }
+  },
+
+  async down (db, client) {
+    apex.store.db = db
+    apex.offlineMode = true // outbox items are queued but not delivered
+
+    const users = await db.collection('users')
+      .find({})
+      .project({ _id: 0, username: 1 })
+      .toArray()
+    const session = client.startSession()
+    try {
+      await session.withTransaction(() => {
+        return Promise.all(users.map(async user => {
+          const actor = await apex.store
+            .getObject(apex.utils.usernameToIRI(user.username), true)
+          console.log(user.username, apex.utils.usernameToIRI(user.username), actor)
+          const { endpoints, streams } = actor
+          delete endpoints[0].proxyUrl
+          delete streams[0].blocklist
+          await apex.store.updateObject({ id: actor.id, endpoints, streams }, actor.id, false)
+          const newActor = await apex.store.getObject(actor.id, true)
+          return apex.publishUpdate(newActor, newActor)
+        }))
+      })
+    } finally {
+      await session.endSession()
+    }
+  }
+}
