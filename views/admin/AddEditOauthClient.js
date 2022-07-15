50151877f3bff3bfbe6d6932d169a953f6c59700
Quinn Madson <quinn.madson@gmail.com>
2022-07-14 21:02:19 -0500

    Login button preview fix.

--- views/admin/AddEditOauthClient.js
+++ views/admin/AddEditOauthClient.js
@@ -92,8 +92,9 @@ export default function AddEditOauthClient ({ showClientList, editId }) {
     }
   }
 
-  function login () {
-    alert('This login button is only a preview.')
+  function login (e) {
+    e.preventDefault()
+    window.alert('This login button is only a preview.')
   }
 
   return (
