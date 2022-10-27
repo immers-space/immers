5ef638cbd03e8955ca7d25ec77a9f60dcf7ee467
Will Murphy <william@datatitian.com>
2022-10-27 10:18:20 -0500

    email input

--- views/components/EmailInput.js
+++ views/components/EmailInput.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function EmailInput () {
   return (
-    <div className='form-item'>
-      <label>E-mail address:</label>
+    <label>
+      E-mail address:
       <input
-        className='aesthetic-windows-95-text-input'
         type='email' name='email'
         required
       />
-    </div>
+    </label>
   )
 }
